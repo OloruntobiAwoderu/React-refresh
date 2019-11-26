3aa6ab60ca2c0aab890b69c97b6febebe8242c05
oloruntobi <awoderuoloruntobi@gmail.com>
2019-11-26 16:41:51 +0100

    added email to the card

--- refresh/src/components/card.jsx
+++ refresh/src/components/card.jsx
@@ -21,7 +21,15 @@ const Div = styled.div`
 const Card = props => {
   return (
     <Div>
+      <img
+        alt="monster"
+        src={`https://robohash.org/${props.monster.id}?set=set2`}
+      />
+
       <h1>{props.monster.name}</h1>
+      <h2>
+        <p>{props.monster.email}</p>
+      </h2>
     </Div>
   );
 };
