98e8fd0f568618b88877f3626c15780ac3ad01c0
oloruntobi <awoderuoloruntobi@gmail.com>
2019-11-27 16:00:30 +0100

    Updates

---
+++ precache-manifest.a7b191a6c5420a11b2b25061057b3ede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df3bc9775c394b54b5a66a69968603d",
+    "url": "/React-refresh/index.html"
+  },
+  {
+    "revision": "2b0016b18ee9438689f2",
+    "url": "/React-refresh/static/css/main.9311a74c.chunk.css"
+  },
+  {
+    "revision": "0c0e48932913cd0cf63c",
+    "url": "/React-refresh/static/js/2.237421ee.chunk.js"
+  },
+  {
+    "revision": "2b0016b18ee9438689f2",
+    "url": "/React-refresh/static/js/main.86151dbc.chunk.js"
+  },
+  {
+    "revision": "521d1bef00e442a15b21",
+    "url": "/React-refresh/static/js/runtime-main.263b68c6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-refresh.github.io/./precache-manifest.334c813bad782082a9b624d2d2450b2d.js"
+  "/React-refresh/precache-manifest.a7b191a6c5420a11b2b25061057b3ede.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-refresh.github.io/./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-refresh/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
